8fd51b45c4f808022b68a86b1e3cfba19fc5ea85
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-07-14 02:22:27 +0900

    modal screen
    basic setups
    modal screen

---
+++ js/basic-modal.js
@@ -0,0 +1,20 @@
+const modalBtn = document.querySelector(".toys-list .toys-toy .modal-button");
+const modalContainer = document.querySelector(".modal-container");
+const closeModalBtn = document.querySelector(".modal-footer button");
+
+const HIDDEN_CLASS = "hidden";
+
+function eraseModal() {
+    modalContainer.classList.add(HIDDEN_CLASS);
+}
+
+function paintModal() {
+    modalContainer.classList.remove(HIDDEN_CLASS);
+}
+
+function init() {
+    modalBtn.addEventListener("click", paintModal);
+    closeModalBtn.addEventListener("click", eraseModal);
+}
+
+init();
